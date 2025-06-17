f467f20cabd5a90bc9691aa5ff14c017551452a4
ayush931 <ayushkumar9315983@gmail.com>
2025-06-17 16:35:25 +0530

    project

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,11 +2,11 @@ import { connectToDB } from "@/config/dbConnection";
 import User from "@/models/user.model";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-export async function POST (request: NextRequest) {
+export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
-  
+
   if (!email || !password) {
     return NextResponse.json(
       { error: "Email and password are required" },
@@ -16,37 +16,32 @@ export async function POST (request: NextRequest) {
 
   await connectToDB();
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
-  console.log(user?.password)
+  console.log(user?.password);
 
   if (!user) {
-    return NextResponse.json(
-      { error: "Please register" },
-      { status: 400 }
-    )
+    return NextResponse.json({ error: "Please register" }, { status: 400 });
   }
 
   const validatePassword = await bcrypt.compare(password, user.password);
 
   if (!validatePassword) {
-    return NextResponse.json(
-      { error: "Invalid Password" },
-      { status: 400 }
-    )
+    return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
+  }
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET not defined");
   }
 
   const token = jwt.sign(
     { id: user._id, email: user.email },
-    process.env.JWT_SECRET as string,
+    process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_EXPIRY || "7d" }
-  )
+  );
 
   if (!token) {
-    return NextResponse.json(
-      { error: "Token not generated" },
-      { status: 400 }
-    )
+    return NextResponse.json({ error: "Token not generated" }, { status: 400 });
   }
 
   const response = NextResponse.json({
@@ -55,20 +50,20 @@ export async function POST (request: NextRequest) {
       id: user._id.toString(),
       name: user.name,
       email: user.email,
-      phone: user.phone
+      phone: user.phone,
     },
     sucess: true,
-    message: "Logged in successfully"
-  })
+    message: "Logged in successfully",
+  });
 
   response.cookies.set({
-    name: 'token',
+    name: "token",
     value: token,
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
     sameSite: true,
-    maxAge: 7 * 24 * 60 * 60 * 1000
-  })
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
   return response;
 }
