6e0a7d596e4c0676157a0f6656c0d344ebe96885
ayush931 <ayushkumar9315983@gmail.com>
2025-06-25 21:52:45 +0530

    added the create company name

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -2,21 +2,27 @@
 
 import { logout } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
+import LoadingButton from './LoadingButton';
 
 export default function LogoutButton() {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const handleSubmit = async () => {
+    setLoading(true);
     const token = localStorage.getItem('token');
 
     if (!token) {
       toast.error('Please login first');
+      setLoading(false);
       return;
     }
     const result = await logout();
 
     if (result?.success) {
       toast.success(result.message);
+      setLoading(false);
       router.push('/');
     }
   };
@@ -26,7 +32,7 @@ export default function LogoutButton() {
       onClick={() => handleSubmit()}
       className="px-5 py-2 rounded-md bg-red-500 hover:bg-red-600 text-white font-[500]"
     >
-      Logout
+      {loading ? <LoadingButton /> : 'Logout'}
     </button>
   );
 }
